3f533b1d2c1506f58af09ae979979dffa1ec3a8e
pn20120162 <pn20120162>
2022-04-28 14:11:24 +0800

    'debug工具'

---
+++ bochs_debug.py
@@ -0,0 +1,6 @@
+import os
+
+if __name__ == '__main__':
+    while 1:
+        os.system('start D:/software/Bochs-2.6/bochsdbg.exe -f D:/software/VirtualBoxVMs/learn-asm/bochsrc.bxrc')
+        input('reboot bochs ? ')
