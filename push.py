b726c78fd74ae4e76d6494bbaf72d1a9c24e845b
pn20120162 <pn20120162>
2022-05-05 10:01:44 +0800

    '修改脚本'

--- push.py
+++ push.py
@@ -4,6 +4,7 @@ if __name__ == '__main__':
     print('start')
     desc = input('please input commit desc ');
 
+    os.system('git pull')
     os.system('git add .')
     os.system('git commit -m \''+desc+'\'')
     os.system('git push')
