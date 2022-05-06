fd86de247da2db9b43b6fb5cf5d56e6690bb9794
pn20120162 <pn20120162>
2022-05-06 15:31:16 +0800

    '习题'

--- booktool/配书源码和工具/c12/c12_mbr.asm
+++ booktool/配书源码和工具/c12/c12_mbr.asm
@@ -2,7 +2,7 @@
          ;文件名：c12_mbr.asm
          ;文件说明：硬盘主引导扇区代码 
          ;创建日期：2011-10-27 22:52
-
+		 ; 第12章检测点12.1 和12.2 答案 https://fishc.com.cn/thread-100616-1-1.html (12.2应该是不能压如双字，解题过程可参考)
          ;设置堆栈段和栈指针 
          mov eax,cs      
          mov ss,eax
@@ -21,20 +21,20 @@
          mov dword [ebx+0x00],0x00000000
          mov dword [ebx+0x04],0x00000000  
 
-         ;创建1#描述符，这是一个数据段，对应0~4GB的线性地址空间
-         mov dword [ebx+0x08],0x0000ffff    ;基地址为0，段界限为0xfffff
-         mov dword [ebx+0x0c],0x00cf9200    ;粒度为4KB，存储器段描述符 
+         ;创建1#描述符，这是一个数据段，对应0~4GB的线性地址空间 -- 段基址 :  0x00000000  段界限:0xfffff
+         mov dword [ebx+0x08],0x0000ffff    ;基地址为0，段界限为0xfffff -- 1111 1111 1111 1111
+         mov dword [ebx+0x0c],0x00cf9200    ;粒度为4KB，存储器段描述符  -- 1100 1111 1001 0010 0000 0000
 
-         ;创建保护模式下初始代码段描述符
-         mov dword [ebx+0x10],0x7c0001ff    ;基地址为0x00007c00，512字节 
+         ;创建保护模式下初始代码段描述符  -- 段基址: 0x00007C00 段界限: 0x001FF
+         mov dword [ebx+0x10],0x7c0001ff    ;基地址为0x00007c00，512字节  -- 对于向上扩展的段来说，段界限在数值上等于段的长度减去1，因此加1是0x200，即512 字节。
          mov dword [ebx+0x14],0x00409800    ;粒度为1个字节，代码段描述符 
 
          ;创建以上代码段的别名描述符
          mov dword [ebx+0x18],0x7c0001ff    ;基地址为0x00007c00，512字节
          mov dword [ebx+0x1c],0x00409200    ;粒度为1个字节，数据段描述符
-
-         mov dword [ebx+0x20],0x7c00fffe
-         mov dword [ebx+0x24],0x00cf9600
+		; -- 栈段 线性基地址是0x00007C00 段界限 0xFFFFE  粒度4KB
+         mov dword [ebx+0x20],0x7c00fffe 
+         mov dword [ebx+0x24],0x00cf9600 
          
          ;初始化描述符表寄存器GDTR
          mov word [cs: pgdt+0x7c00],39      ;描述符表的界限   
@@ -56,7 +56,7 @@
                                              
          [bits 32]                          
   flush:                                     
-         mov eax,0x0018                      
+         mov eax,0x0018                     ; -- 11 000 ,选择子= 3 ，即 代码段的别名段选择子
          mov ds,eax
       
          mov eax,0x0008                     ;加载数据段(0..4GB)选择子
@@ -64,11 +64,11 @@
          mov fs,eax
          mov gs,eax
       
-         mov eax,0x0020                     ;0000 0000 0010 0000
+         mov eax,0x0020                     ;0000 0000 0010 0000 -- 选择子 = 4 即栈段选择子
          mov ss,eax
-         xor esp,esp                        ;ESP <- 0
+         xor esp,esp                        ;ESP <- 0   -- 清零
       
-         mov dword [es:0x0b8000],0x072e0750 ;字符'P'、'.'及其显示属性
+         mov dword [es:0x0b8000],0x072e0750 ;字符'P'、'.'及其显示属性  -- 高位是属性(颜色、背景)，低位是字符， “072e” 07是属性 ，2e是 '.'
          mov dword [es:0x0b8004],0x072e074d ;字符'M'、'.'及其显示属性
          mov dword [es:0x0b8008],0x07200720 ;两个空白字符及其显示属性
          mov dword [es:0x0b800c],0x076b076f ;字符'o'、'k'及其显示属性
@@ -81,11 +81,11 @@
   @@2:                                      ;是后面的32位 
          mov ax,[string+bx] 
          cmp ah,al                          ;ah中存放的是源字的高字节 
-         jge @@3 
-         xchg al,ah 
-         mov [string+bx],ax 
+         jge @@3 							; 若大于或等于则转移
+         xchg al,ah                         ;-- xchg 交换指令 交换两个操作数的内容
+         mov [string+bx],ax                  ; -- 重新赋值(覆盖)
   @@3:
-         inc bx 
+         inc bx                             ; -- 下一个字
          loop @@2 
          pop ecx 
          loop @@1
@@ -93,7 +93,7 @@
          mov ecx,pgdt-string
          xor ebx,ebx                        ;偏移地址是32位的情况 
   @@4:                                      ;32位的偏移具有更大的灵活性
-         mov ah,0x07
+         mov ah,0x07						; -- 字符属性
          mov al,[string+ebx]
          mov [es:0xb80a0+ebx*2],ax          ;演示0~4GB寻址。
          inc ebx
---
+++ booktool/配书源码和工具/c12/c12_mbr_example.asm
@@ -0,0 +1,191 @@
+    ;代码清单12-1
+    ;文件名：ex12-1.asm
+    ;文件说明：硬盘主引导扇区代码  参考 https://www.jianshu.com/p/34c0d3e350c3  https://blog.csdn.net/longintchar/article/details/50878960
+    ;创建日期：16:23 2018/5/30
+
+;---------------------------------------------------------------    
+;定义常量
+;---------------------------------------------------------------    
+    MEMORY_START equ 0x100000           ;要检测的内存起始地址
+    MEMORY_END   equ 0x500000           ;要检测的内存结束地址
+    MEMORY_SIZE  equ (MEMORY_END-MEMORY_START)/4    ;以双字位单元
+;---------------------------------------------------------------    
+    
+    ;设置堆栈段和栈指针
+    mov eax,cs
+    mov ss,eax
+    mov sp,0x7c00
+    
+    ;计算GDT所在的逻辑段地址
+    mov eax,[cs:pdgt+0x7c00+0x02]   ;GDT的32位线性基地址
+    xor edx,edx
+    mov ebx,16
+    div ebx                         ;分解成16位逻辑地址
+    
+    mov ds,eax          ;令DS指向该段以进行操作：EAX低16位有效 DS=0x7e00
+    mov ebx,edx         ;段内起始偏移地址：EDX EBX低16位有效 ebx=0x0000
+    
+    ;创建0#描述符,它是空描述符,这是处理器的要求
+    mov dword [ebx+0x00],0x00000000
+    mov dword [ebx+0x04],0x00000000
+    
+    ;创建1#描述符，这是一个数据段，对应0~4GB的线性地址空间
+    mov dword [ebx+0x08],0x0000ffff     ;段基地址0x00000000
+    mov dword [ebx+0x0c],0x00cf9200
+    
+    ;创建2#描述符，这是一个只执行的代码段
+    mov dword [ebx+0x10],0x7c0001ff     ;段基地址0x00007C00
+    mov dword [ebx+0x14],0x00409800
+    
+    ;创建3#描述符，这是上面代码段的别名
+    mov dword [ebx+0x18],0x7c0001ff     ;段基地址0x00007C00
+    mov dword [ebx+0x1c],0x00409200
+    
+    ;创建4#描述符，这是栈段
+    mov dword [ebx+0x20],0x7c00fffe     ;段基地址0x00007C00
+    mov dword [ebx+0x24],0x00cf9600
+    
+    ;初始化描述符寄存器GDTR
+    mov word [cs:pdgt+0x7c00],39        ;5*8-1=39
+    lgdt [cs:pdgt+0x7c00]
+    
+    in al,0x92                          ;南桥芯片的端口
+    or al,0000_0010B
+    out 0x92,al                         ;打开A20
+    
+    cli
+    
+    mov eax,cr0
+    or eax,1
+    mov cr0,eax                         ;设置PE位
+    
+    ;以下进入保护模式... ...
+    jmp dword 0x0010:flush
+    
+    [bits 32]
+flush:
+    mov eax,0x0018      ;索引号3#
+    mov ds,eax      
+    
+    mov eax,0x0008      ;索引号1#
+    mov es,eax
+    mov fs,eax
+    mov gs,eax
+    
+    mov eax,0x0020      ;索引号4#
+    mov ss,eax
+    xor esp,esp         ;ESP=0
+    
+    mov dword [es:0x0b8000],0x072e0750  ;'P.'
+    mov dword [es:0x0b8004],0x072e074d  ;'M.'
+    mov dword [es:0x0b8008],0x07200720  ;'  '
+    mov dword [es:0x0b800c],0x076b076f  ;'ok'
+
+    
+;---------------------------------------------------------------    
+;显示需要检测的总的单元个数
+;---------------------------------------------------------------        
+    mov byte [es:0x0b8140],'H'
+    mov byte [es:0x0b8142],'E'
+    mov byte [es:0x0b8144],'X'
+    mov byte [es:0x0b8146],':'
+    
+    mov ebp,0x0b8140+10
+    mov ecx,0
+    call check
+    
+    mov byte [es:0x0b8140+30],'/'
+    
+    mov ebp,0x0b8140+34
+    mov ecx,MEMORY_SIZE
+    call check
+	 
+;---------------------------------------------------------------        
+;内存检测
+;以双字为单元，使用花码0x55aa55aa和0xaa55aa55进行内存检测
+;---------------------------------------------------------------        
+        
+        xor ecx,ecx                 ;检测的单元个数
+        mov ebx,MEMORY_START        ;检测的起始地址
+	   
+exam:   
+        mov dword [es:ebx],0x55aa55aa
+        cmp dword [es:ebx],0x55aa55aa
+        jnz err
+        
+        mov dword [es:ebx],0xaa55aa55
+        cmp dword [es:ebx],0xaa55aa55
+        jnz err
+
+        add ebx,4
+        inc ecx
+        mov byte  [es:0x0b80a0+28],'!'
+        not byte  [es:0x0b80a0+29]
+        
+        
+        mov ebp,0x0b8140+10
+        call check
+        
+        cmp ebx,MEMORY_END
+        jnz exam
+        
+        mov dword [es:0x0b80b0+20],0x076b076f       ;'ok'
+        
+;---------------------------------------------------------------        
+err:
+        hlt                         ;进入停机状态
+
+        
+;---------------------------------------------------------------
+;子程序：check
+;参数：    
+;       ecx = 要显示的数值
+;       ebp = 数值在显存的起始位置
+;功能：    计算并显示检测的内存个数(以双字位单位)
+;---------------------------------------------------------------
+check:          push ebx
+                push ecx
+                push esi
+                push eax
+                push ebp
+                
+                mov eax,ecx
+                xor ebx,ebx
+                mov ecx,8
+                mov esi,16
+        digit:                 ; digit 和 show有对应关系 ，digit算出十六进制  ，show根据内存地址显示
+                xor edx,edx
+                div esi
+                mov [mem+ebx],dl ; 得到余数，这个好像是在转成十六进制
+                inc ebx
+                loop digit
+                
+                
+                xor edi,edi
+                xor ebx,ebx
+                mov esi,7
+        show:   
+                mov al,[mem+esi]
+                mov bl,al          ; bl   下面ebx的低8位
+                mov al,[number+ebx]
+                mov [es:ebp+edi],al
+                add edi,2
+                dec esi
+                jns show
+        
+                pop ebp
+                pop eax
+                pop esi
+                pop ecx
+                pop ebx
+        
+ret
+;---------------------------------------------------------------
+    mem     db  0,0,0,0,0,0,0,0     ;存放数位
+    number  db '0123456789ABCDEF'
+;---------------------------------------------------------------
+    pdgt    dw  0
+            dd  0x00007e00          ;GDT的物理地址
+;---------------------------------------------------------------
+    times 510-($-$$) db 0
+                     db 0x55,0xaa
